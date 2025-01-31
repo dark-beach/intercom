3884eba940e821df79c13f7b9e1b8a55efa77d84
dark beach <35419365+dark-beach@users.noreply.github.com>
2025-01-31 09:27:56 +0000

    Add files via upload

--- main pc.py
+++ main pc.py
@@ -1,11 +1,11 @@
 # run this on the computer that sends messages to server
 import socket
 
-SERVER_IP = "192.168.X.X"  # LLM's IP
+SERVER_IP = "192.168.X.X"  # server IP
 PORT = 5000 # all port listen
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect((SERVER_IP, PORT)) # change to server's ip and port
+client_socket.connect((SERVER_IP, PORT))
 
 command = "open_notepad"  # just a test command, has to match server main's command list
 client_socket.send(command.encode("utf-8"))
